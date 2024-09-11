5be94d05ccbfe86f1c8aeeb98fecce684d8fd149
Ismail <126619125+is9700@users.noreply.github.com>
2024-09-11 14:48:34 +0200

    Update OptimizedThreading.java

--- OptimizedThreading.java
+++ OptimizedThreading.java
@@ -1,32 +1,31 @@
-package optimized;
-
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class OptimizedThreading {
-
     public static void main(String[] args) {
-//Threadpool erstellen
         ExecutorService executor = Executors.newFixedThreadPool(5);
-    //fur jede zahl iùm bereich erzeugt ein CompletableFuture-Objekt(asynchron)
-        List<CompletableFuture<MyCallable>> futures = IntStream.rangeClosed(1, 10)
-                .mapToObj(i -> CompletableFuture.supplyAsync(() -> new MyCallable("Task " + i, i * 100), executor))//mit Lambda
-                .collect(Collectors.toList());//Sammelt die CompletableFuture-Objekte in eine Liste
 
+        List<CompletableFuture<String>> futures = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return new MyCallable("Task " + i, i * 100).call();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return "";
+                    }
+                }, executor))
+                .collect(Collectors.toList());
         List<String> results = futures.stream()
                 .map(CompletableFuture::join)
-                .map(MyCallable::getResult)
                 .collect(Collectors.toList());
-
         executor.shutdown();
-
-        results.forEach(System.out::println);//ausgabe
+        results.forEach(System.out::println);
     }
 }
 
-class MyCallable implements Callable<MyCallable> {
+class MyCallable implements Callable<String> {
     private final String taskName;
     private final int computationTime;
 
@@ -36,15 +35,13 @@ class MyCallable implements Callable<MyCallable> {
     }
 
     @Override
-    public MyCallable call() throws InterruptedException {//hier MyCallable Object als Ausgabe statt String
-        System.out.println("Task " + taskName + " started by thread: " + Thread.currentThread().getName());
-
-        TimeUnit.MILLISECONDS.sleep(computationTime);
-        System.out.println("Task " + taskName + " completed by thread: " + Thread.currentThread().getName());
-        return this;
-    }
-
-    public String getResult() {//ausgabe 
-        return "Task " + taskName + " completed by thread: " + Thread.currentThread().getName();
+    public String call() throws InterruptedException {
+        System.out.println("Task " + taskName + " started by thread: " +
+                Thread.currentThread().getName());
+        Thread.sleep(computationTime);
+        return "Task " + taskName + " completed by thread: " +
+                Thread.currentThread().getName();
     }
 }
+
+
